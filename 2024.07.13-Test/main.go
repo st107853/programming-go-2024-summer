e18fbab1ee758162ee8562f1aa58419a30503875
Kateryna Luhova <katya@DESKTOP-GML8N6E>
2024-07-24 19:44:45 +0300

    finish version

--- 2024.07.13-Test/main.go
+++ 2024.07.13-Test/main.go
@@ -2,31 +2,49 @@ package main
 
 import (
 	"database/sql"
+	"encoding/base64"
+	"errors"
+	"fmt"
 	"log"
+	"math/rand"
+	"os"
 
-	jwtware "github.com/gofiber/contrib/jwt"
+	//	"github.com/go-gomail/gomail"
+	"github.com/go-sql-driver/mysql"
 	"github.com/gofiber/fiber/v2"
 	jwt "github.com/golang-jwt/jwt/v5"
 	"github.com/sirupsen/logrus"
 )
 
-const (
-	contextKeyUser = "ip"
-)
-
 func main() {
 	app := fiber.New()
 
-	app.Use(jwtware.New(jwtware.Config{
-		SigningKey: jwtware.SigningKey{
-			Key: jwtSecretKey,
-		},
-		ContextKey: contextKeyUser,
-	}))
-	app.Get("/profile", Profile)
-	app.Post("/refresh", Refresh)
+	app.Get("/profile/:guid", Profile)
+	app.Post("/refresh/:guid/:token", Refresh)
 
 	logrus.Fatal(app.Listen(":8080"))
+
+	//Capture connection propeties.
+	cfg := mysql.Config{
+		User:   os.Getenv("DBUSER"),
+		Passwd: os.Getenv("DBPASS"),
+		Net:    "tcp",
+		Addr:   "localhost:3306",
+		DBName: "guids",
+	}
+	//Get a database handle.
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Connected!")
 }
 
 var db *sql.DB
@@ -37,48 +55,113 @@ type User struct {
 	IP    string
 }
 
-// Структура HTTP-ответа на вход в аккаунт
-// В ответе содержится JWT-токен авторизованного пользователя
-type LoginResponse struct {
-	AccessToken string `json:"access_token"`
-}
-
-// Секретный ключ для подписи JWT-токена
-// Необходимо хранить в безопасном месте
-var jwtSecretKey = []byte("very-secret-key")
-
-// Обработчик HTTP-запросов на вход в аккаунт
+// Handler of http requests for token updates
 func Refresh(c *fiber.Ctx) error {
 	var user User
-	userGuid := c.Context().Value("guid")
-	//	refreshToken := c.Context().Value("token")
 
-	// Ищем пользователя в памяти приложения по электронной почте
-	row := db.QueryRow("SELECT * FROM users WHERE guid = ?", userGuid)
-	// Если пользователь не найден, возвращаем ошибку
+	userGuid := c.Params("guid")
+
+	if userGuid == "" {
+		return c.SendStatus(fiber.StatusUnauthorized)
+	}
+
+	//Getting data from MySql
+	row := db.QueryRow("SELECT * FROM users WHERE guids = ?", userGuid)
+
 	if err := row.Scan(&user.GUID, &user.Email, &user.IP); err != nil {
 		return err
 	}
 
+	//Encoding the refresh token
+	base64Token := c.Params("token")
+	token, err := base64.StdEncoding.DecodeString(base64Token)
+	if err != nil {
+		return err
+	}
+
+	if string(token) != refreshToken(user) {
+		return errors.New("wrong refresh token")
+	}
+	/*
+		//Sending an email from to the mail if the user's IP has changed
+			if user.IP != string(c.Context().LocalIP()) {
+				fmt.Println("user ip pu-pu-pu")
+				user.IP = string(c.Context().LocalIP())
+				m := gomail.NewMessage()
+				m.SetHeader("From", "alex@example.com")
+				m.SetHeader("To", user.Email)
+				m.SetHeader("Subject", "Hello!")
+				m.SetBody("text/html", "Warning! You changed your ip and token.")
+
+				d := gomail.NewDialer("test.com/m", 587, "user", "123456")
+
+				//Send the email to User
+				if err := d.DialAndSend(m); err != nil {
+					panic(err)
+				}
+			}
+	*/
+	jwtSecretKey = randString(16)
+
 	return nil
 }
 
-// Структура HTTP-ответа с информацией о пользователе
+// The structure of the HTTP response with tokens information
 type ProfileResponse struct {
-	JWT string `json:"jwt"`
+	Access  string `json:"jwtAccess"`
+	Refresh string `json:"jwtRefresh"`
 }
 
-func jwtFromUser(user User) string {
-	// Генерируем JWT-токен для пользователя,
-	// который он будет использовать в будущих HTTP-запросах
+// Handler for HTTP requests for user information
+func Profile(c *fiber.Ctx) error {
+	var user User
 
-	// Генерируем полезные данные, которые будут храниться в токене
+	userGuid := c.Params("guid")
+
+	fmt.Println(userGuid)
+	if userGuid == "" {
+		return c.SendStatus(fiber.StatusUnauthorized)
+	}
+
+	//Getting data from MySql
+	row := db.QueryRow("SELECT * FROM users WHERE guids = ?", userGuid)
+
+	if err := row.Scan(&user.GUID, &user.Email, &user.IP); err != nil {
+		return err
+	}
+
+	base64Token := base64.StdEncoding.EncodeToString([]byte(refreshToken(user)))
+
+	return c.JSON(ProfileResponse{
+		Access:  jwtFromUser(user),
+		Refresh: base64Token,
+	})
+}
+
+// The secret key for signing the JWT token
+var jwtSecretKey = []byte("very-secret-key")
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+// Updating the key after refresh
+func randString(n int) []byte {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Int63()%int64(len(letterBytes))]
+	}
+	return b
+}
+
+// Generating a JWT token
+func jwtFromUser(user User) string {
+
+	//Generating useful data that will be stored in the token
 	payload := jwt.MapClaims{
 		"sub": user.Email,
 		"ip":  user.IP,
 	}
 
-	// Создаем новый JWT-токен и подписываем его по алгоритму HS256
+	//Creating a new JWT token and signing it with the HS512 algorithm
 	token := jwt.NewWithClaims(jwt.SigningMethodHS512, payload)
 
 	t, err := token.SignedString(jwtSecretKey)
@@ -90,22 +173,22 @@ func jwtFromUser(user User) string {
 	return t
 }
 
-// Обработчик HTTP-запросов на получение информации о пользователе
-func Profile(c *fiber.Ctx) error {
-	var user User
+// Generating a JWT refresh token
+func refreshToken(user User) string {
 
-	userGuid := c.Params("guid", "")
-	if userGuid == "" {
-		return c.SendStatus(fiber.StatusUnauthorized)
+	//Generating useful data that will be stored in the token
+	payload := jwt.MapClaims{
+		"sub": user.GUID,
 	}
 
-	row := db.QueryRow("SELECT * FROM users WHERE guid = ?", userGuid)
+	//Creating a new JWT token and signing it with the HS512 algorithm
+	token := jwt.NewWithClaims(jwt.SigningMethodHS512, payload)
 
-	if err := row.Scan(&user.GUID, &user.Email, &user.IP); err != nil {
-		return err
+	t, err := token.SignedString(jwtSecretKey)
+	if err != nil {
+		log.Fatal(err)
+		return ""
 	}
 
-	return c.JSON(ProfileResponse{
-		JWT: jwtFromUser(user),
-	})
+	return t
 }
--- 2024.07.13-Test/table_creater.sql
+++ 2024.07.13-Test/table_creater.sql
@@ -1,14 +1,13 @@
-DROP TABLE IF EXISTS users;
-CREATE TABLE users (
-    guid VARCHAR(128) NOT NULL,
+CREATE TABLE IF NOT EXISTS users (
+    guids VARCHAR(128) NOT NULL,
     email VARCHAR(128) NOT NULL,
     ip VARCHAR(128) NOT NULL,
-    PRIMARY KEY (`guid`)
+    PRIMARY KEY (guids)
 );
 
 INSERT INTO users
-    (guid, email, ip)
-VALUE
+    (guids, email, ip)
+VALUES
     ('c15dcfee-adfe-4874-9e59-73d54335d214', 'johndoe@gm.com', '217.197.2.10'),
     ('8415c0f5-bf40-4ab2-a230-64db5b080f45', 'johndoe@gm.com', '217.197.2.10'),
     ('6cc105f6-846e-408e-9a79-35008aeb2ddd', 'johndoe@gm.com', '217.197.2.10'),
