9858ad56d81b0096ecd38351ae58e1f5ef39336d
Kateryna Luhova <katya@DESKTOP-GML8N6E>
2024-08-04 20:34:23 +0300

    a bit fix

---
+++ 2024.08.01-Test/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"math/rand/v2"
+	"os"
+	"runtime"
+	"strconv"
+
+	"algorithm.com/naive"
+)
+
+func main() {
+
+	//	Create(10_000_000)
+
+	var memStats runtime.MemStats
+
+	runtime.ReadMemStats(&memStats)
+
+	res := naive.FileScaner("./test.txt")
+
+	fmt.Println(res)
+
+	fmt.Printf("Total allocated memory (in bytes): %d\n", memStats.Alloc)
+	fmt.Printf("Heap memory (in bytes): %d\n", memStats.HeapAlloc)
+	fmt.Printf("Number of garbage collections: %d\n", memStats.NumGC)
+}
+
+func Create(n int) {
+	file, err := os.Create("test.txt")
+
+	if err != nil {
+		fmt.Println("Unable to create")
+	}
+
+	defer file.Close()
+
+	for i := 0; i < n; i++ {
+		text := ""
+
+		for j := 0; j < 4; j++ {
+			text += strconv.Itoa(rand.IntN(255)) + "."
+		}
+
+		file.WriteString(text[:len(text)-1] + "\n")
+	}
+}
