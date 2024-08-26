ef3a70b6ca9c60337513791c33b9c1451c8c0889
Kateryna Luhova <katya@DESKTOP-GML8N6E>
2024-08-27 00:03:51 +0300

    add logger

--- 2024.08.22-Cloud-Native/main.go
+++ 2024.08.22-Cloud-Native/main.go
@@ -1,17 +1,53 @@
 package main
 
 import (
+	"bufio"
 	"errors"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"os"
+	"sync"
 
 	"github.com/gorilla/mux"
 )
 
-var store = make(map[string]string)
+const (
+	_                     = iota
+	EventDelete EventType = iota
+	EventPut
+	EventGet
+)
+
+type EventType byte
+
+type TransactionLogger interface {
+	WriteDelete(key string)
+	WritePut(key, value string)
+	WriteGet(key string)
+	Err() <-chan error
+
+	ReadEvents() (<-chan Event, <-chan error)
+
+	Run()
+}
+
+type Event struct {
+	Sequence  uint64    // A unique record ID
+	EventType EventType //The action token
+	Key       string    // The key affected by this transaction
+	Value     string    // The value of a PUT the transaction
+}
 
 var ErrorNoSuchKey = errors.New("no such key")
+
+var logger TransactionLogger
+
+var store = struct {
+	sync.RWMutex
+	m map[string]string
+}{m: make(map[string]string)}
 
 func main() {
 	r := mux.NewRouter()
@@ -19,6 +55,8 @@ func main() {
 	r.HandleFunc("/v1/{key}", keyValuePutHandler).Methods("PUT")
 	r.HandleFunc("/v1/{key}", keyValueGetHandler).Methods("GET")
 	r.HandleFunc("/v1/{key}", keyValueDeleteHandler).Methods("DELETE")
+
+	initializeTransactionLog()
 
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
@@ -33,6 +71,7 @@ func keyValueDeleteHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	logger.WriteDelete(key)
 	w.WriteHeader(http.StatusOK)
 }
 
@@ -50,6 +89,7 @@ func keyValueGetHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	logger.WriteGet(key)
 	w.Write([]byte(value))
 }
 
@@ -68,19 +108,25 @@ func keyValuePutHandler(w http.ResponseWriter, r *http.Request) {
 	err = Put(key, string(value))
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
+	logger.WritePut(key, string(value))
 	w.WriteHeader(http.StatusCreated)
 }
 
 func Put(key string, value string) error {
-	store[key] = value
+	store.Lock()
+	store.m[key] = value
+	store.Unlock()
 
 	return nil
 }
 
 func Get(key string) (string, error) {
-	value, ok := store[key]
+	store.RLock()
+	value, ok := store.m[key]
+	store.RUnlock()
 
 	if !ok {
 		return "", ErrorNoSuchKey
@@ -90,7 +136,128 @@ func Get(key string) (string, error) {
 }
 
 func Delete(key string) error {
-	delete(store, key)
+	store.Lock()
+	delete(store.m, key)
+	store.Unlock()
 
 	return nil
+}
+
+type FileTransactionLogger struct {
+	events       chan<- Event // Write-only channel for sending events
+	errors       <-chan error // Read-only channel for receiving errors
+	lastSequence uint64       // The last used event sequece number
+	file         *os.File     // The location of the transaction log
+}
+
+func (l *FileTransactionLogger) WritePut(key, value string) {
+	l.events <- Event{EventType: EventPut, Key: key, Value: value}
+}
+
+func (l *FileTransactionLogger) WriteDelete(key string) {
+	l.events <- Event{EventType: EventDelete, Key: key}
+}
+
+func (l *FileTransactionLogger) WriteGet(key string) {
+	l.events <- Event{EventType: EventGet, Key: key}
+}
+
+func (l *FileTransactionLogger) Err() <-chan error {
+	return l.errors
+}
+
+func NewFileTransactionLogger(filename string) (TransactionLogger, error) {
+	file, err := os.OpenFile(filename, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0755)
+	if err != nil {
+		return nil, fmt.Errorf("cannot open transaction log file: %w", err)
+	}
+
+	return &FileTransactionLogger{file: file}, nil
+}
+
+func (l *FileTransactionLogger) Run() {
+	events := make(chan Event, 16)
+	l.events = events
+
+	errors := make(chan error, 1)
+	l.errors = errors
+
+	go func() {
+		for e := range events {
+			l.lastSequence++
+
+			_, err := fmt.Fprintf(
+				l.file,
+				"%d\t%d\t%s\t%s\n",
+				l.lastSequence, e.EventType, e.Key, e.Value)
+
+			if err != nil {
+				errors <- err
+				return
+			}
+		}
+	}()
+}
+
+func (l *FileTransactionLogger) ReadEvents() (<-chan Event, <-chan error) {
+	scanner := bufio.NewScanner(l.file) // Create a Scanner for l.file
+	outEvent := make(chan Event)        // An unbuffered Event channel
+	outError := make(chan error, 1)     // A buffered error channel
+	go func() {
+		var e Event
+		defer close(outEvent) // Close the channels when the
+		defer close(outError) // goroutine ends
+		for scanner.Scan() {
+			line := scanner.Text()
+			if _, err := fmt.Sscanf(line, "%d\t%d\t%s\t%s",
+				&e.Sequence, &e.EventType, &e.Key, &e.Value); err != nil {
+				outError <- fmt.Errorf("input parse error: %w", err)
+				return
+			}
+			// Sanity check! Are the sequence numbers in increasing order?
+			if l.lastSequence >= e.Sequence {
+				outError <- fmt.Errorf("transaction numbers out of sequence")
+				return
+			}
+			l.lastSequence = e.Sequence // Update last used sequence #
+			outEvent <- e               // Send the event along
+		}
+		if err := scanner.Err(); err != nil {
+			outError <- fmt.Errorf("transaction log read failure: %w", err)
+			return
+		}
+	}()
+	return outEvent, outError
+}
+
+func initializeTransactionLog() error {
+	var err error
+
+	logger, err = NewFileTransactionLogger("transaction.log")
+	if err != nil {
+		return fmt.Errorf("field to create event logger: %w", err)
+	}
+
+	events, errors := logger.ReadEvents()
+	e, ok := Event{}, true
+
+	for ok && err == nil {
+		select {
+		case err, ok = <-errors: // Retrieve any errors
+		case e, ok = <-events:
+			switch e.EventType {
+			case EventDelete: // Got a DELETE event
+				err = Delete(e.Key)
+			case EventPut: // Got a PUT event
+				err = Put(e.Key, e.Value)
+			case EventGet: // Got a GET event
+				_, err = Get(e.Key)
+			}
+
+		}
+	}
+
+	logger.Run()
+
+	return err
 }
